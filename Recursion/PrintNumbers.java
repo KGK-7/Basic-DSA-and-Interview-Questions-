e74db7c29f726257fc7dc3b3d80273e9c40f830f
KGK-7 <tamilangokul58@gmail.com>
2025-08-04 06:52:24 +0530

    feat : Java programs utilizing recursion

---
+++ Recursion/PrintNumbers.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class PrintNumbers {
+    public static void printNumbers(int n) {
+        if (n == 0) return;
+        printNumbers(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        printNumbers(5);
+    }
+}
