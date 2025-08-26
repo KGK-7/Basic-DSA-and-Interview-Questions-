e213bff0313fb160d507a4ad623ec7a02eb838ac
KGK-7 <tamilangokul58@gmail.com>
2025-08-26 16:33:01 +0530

    feat: java solution for sum of digits

---
+++ Java Training/SumOfDigits.java
@@ -0,0 +1,14 @@
+public class SumOfDigits {
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int number = 12345;
+        System.out.println("Sum of digits of " + number + " is: " + sumOfDigits(number));
+    }
+}
