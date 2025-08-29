80777414f821e7cf8639b014760735eec9d1bfdc
KGK-7 <tamilangokul58@gmail.com>
2025-08-29 20:12:39 +0530

    feat: java code for unit matrix

---
+++ Java Training/UnitMatEx.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class UnitMatEx{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter range of the square matrix");
+		int n = sc.nextInt();
+		int arr[][] = new int[n][n];
+		System.out.println("Enter matrix elements");
+		for(int i= 0 ; i<n; i++){
+			for(int j=0 ; j<n ; j++){
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Matrix Elements are: ");
+		for(int i= 0 ; i<n; i++){
+			for(int j=0 ; j<n ; j++){
+				System.out.println(arr[i][j] + " ");
+			}
+		}
+		boolean unit_mat = true;
+		for(int i= 0 ; i<n; i++){
+			for(int j=0 ; j<n ; j++){
+				if( i==j && arr[i][j] != 1){
+					unit_mat = false;
+
+				}
+				else if( i != j && arr[i][j] != 0){
+					unit_mat = false;
+				}
+			}
+		}
+		if(unit_mat){
+			System.out.println("The given matrix is an unit matrix ");
+		}
+		else{
+			System.out.println("The given matrix is not an unit matrix ");
+		}
+}
+}
+
+
+		
+				
