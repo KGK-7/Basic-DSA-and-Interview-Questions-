42b7a8f63ac0791a20f4a0112eb04faba8997e0b
KGK-7 <tamilangokul58@gmail.com>
2025-08-31 22:00:17 +0530

    feat: java code for vector ds example

---
+++ Java Training/VectorEx.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class VectorEx{
+		public static void main(String[] args){
+			Vector<Integer> arr = new Vector<>();
+			arr.add(7);
+			//arr.add("heloo");
+			arr.add(12);
+			arr.add(7);
+			arr.add(19);
+			//arr.add("Apple");
+			arr.add(19);
+
+			arr.add(19);
+
+			arr.add(19);
+arr.add(19);
+arr.add(19);
+arr.add(19);
+arr.add(19);
+//arr.add(19);
+//arr.add(19);
+
+			System.out.println(arr);
+			System.out.println(arr.size());
+
+			System.out.println(arr.capacity());
+
+}
+}
